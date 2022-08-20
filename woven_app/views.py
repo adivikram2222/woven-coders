d984c1c504071f8b0f5be4443e39097429ee71b0
Aditya vikram <69596980+adivikram2222@users.noreply.github.com>
2022-08-21 01:45:06 +0530

    Add files via upload

---
+++ woven_app/views.py
@@ -0,0 +1,26 @@
+from http.client import HTTPResponse
+from django.http import HttpRequest
+from django.shortcuts import render
+from woven_app.models import Contact
+
+# Create your views here.
+def index(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        project= request.POST.get('project')
+        message = request.POST.get('message')
+        contact = Contact(name=name, email=email, project=project, message = message)
+        contact.save()
+    return render(request, 'index.html')
+def login(request):
+    return render(request, 'login_signup.html')
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        project= request.POST.get('project')
+        message = request.POST.get('message')
+        contact = Contact(name=name, email=email, project=project, message=message)
+        contact.save()
+    return HTTPResponse('your message has been submitted successfully')
