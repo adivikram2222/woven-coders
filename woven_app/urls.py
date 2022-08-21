a1cdf92c150bef990ef4e9b42faab3fb35ba12a7
Shweta kumari <85580523+shweta2222@users.noreply.github.com>
2022-08-21 23:46:24 +0530

    fgv

--- woven_app/urls.py
+++ woven_app/urls.py
@@ -4,8 +4,9 @@ from woven_app import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('login', views.login, name="login"),
+    path('apply/', views.signup, name="login"),
     path('contact/',views.contact,name="contact"),
+    
     
     
 ]
