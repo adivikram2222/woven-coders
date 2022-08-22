ba334514cdb036610a637e7483994dfe09a76f4c
technophileADITYA <108268286+technophileADITYA@users.noreply.github.com>
2022-08-22 21:27:24 +0530

    up js

--- static/index.js
+++ static/index.js
@@ -36,3 +36,45 @@ $(document).ready(function(){
   });
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+const input = document.querySelector("input"),
+emailIcon = document.querySelector(".email-icon")
+
+input.addEventListener("keyup", () => {
+let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+if (input.value === "") {
+    emailIcon.classList.replace("uil-check-circle", "uil-envelope");
+    return emailIcon.style.color = "#b4b4b4";
+}
+if (input.value.match(pattern)) {
+    emailIcon.classList.replace("uil-envelope", "uil-check-circle");
+    return emailIcon.style.color = "#4bb543"
+}
+emailIcon.classList.replace("uil-check-circle", "uil-envelope");
+emailIcon.style.color = "#de0611"
+})
+
+function checkPassword() {
+var password = document.getElementById("password").value;
+var cPassword = document.getElementById("c-password").value;
+var message = document.getElementById("message");
+if (password == cPassword) {
+    message.textContent = "Passwords Match";
+    message.style.backgroundColor = "green";
+} else {
+    message.textContent = "Passwords don't Match";
+    message.style.backgroundColor = "red";
+}
+}
